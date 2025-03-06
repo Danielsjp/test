44284b17c6d1a889390ba709eaacd426b0839e2d
daniel.sanchez <sjp.daniel@gmail.com>
2025-03-06 10:05:30 +0100

    test

---
+++ PRUEBA GIT.ps1
@@ -0,0 +1,12 @@
+﻿#git config --global user.name daniel.sanchez
+git config --global user.email sjp.daniel@gmail.com
+git config --global core.editor "code --wait"
+git init
+git status
+git add .
+git remote add origin https://github.com/Danielsjp/test.git
+git branch -M main 
+git push -u origin main
+git add .
+git commit -m "test"
+git push
